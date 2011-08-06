76ec2c2b759ede3f23149abd4f366bdf650d2759
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-08-06 22:15:29 +0200

    Adding a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebabytime::Application.config.secret_token = '82a0573cec44080d4da9f16eac7105adf31a6996d12a5452378991433b66e2919b63c7a11de56a3f068379adfa356b552726782438a224ab9c8b687bcdbfb910'
